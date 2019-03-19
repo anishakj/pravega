1fefc3b3404fce4484142068f19398d193bf59bd
anishakj <anisha.kj@dell.com>
2019-03-19 04:17:37 +0000

    added logs
    Signed-off-by: anishakj <anisha.kj@dell.com>

--- controller/src/main/java/io/pravega/controller/server/Main.java
+++ controller/src/main/java/io/pravega/controller/server/Main.java
@@ -25,16 +25,33 @@ import io.pravega.controller.timeout.TimeoutServiceConfig;
 import io.pravega.controller.util.Config;
 import io.pravega.shared.metrics.MetricsProvider;
 import io.pravega.shared.metrics.StatsProvider;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 
+import static java.net.NetworkInterface.getNetworkInterfaces;
 /**
  * Entry point of controller server.
  */
 @Slf4j
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
+	log.info("======> IP details");
+        Enumeration en = getNetworkInterfaces();
+        while (en.hasMoreElements()) {
+            NetworkInterface n = (NetworkInterface) en.nextElement();
+            Enumeration ee = n.getInetAddresses();
+            while (ee.hasMoreElements()) {
+                InetAddress i = (InetAddress) ee.nextElement();
+                System.out.println(i.getHostAddress());
+                log.info("===> IP details: {}", i.getHostAddress());
+            }
+        }
+        log.info("======> End of fetch ip");
 
         StatsProvider statsProvider = null;
         try {
